5bc262643dc2c7d38fe161321f81207585c95d13
Jaspreet Singh <32124127+JaspreetRFSingh@users.noreply.github.com>
2018-12-20 22:05:40 +0530

    Optimized Anagram algorithm

--- src/Anagram.java
+++ src/Anagram.java
@@ -1,25 +1,23 @@
 public class Anagram {
 
-	private static boolean isAnagram(String s1, String s2) {
-        int []ca = new int[26];
-        int []cb = new int[26];
-
-        for (char c : s1.toCharArray()) {
-            ca[c - 'a']++;
-        }
-
-        for (char c : s2.toCharArray()) {
-            cb[c - 'a']++;
-        }
-
-        for (int i = 0; i < 26; i++) {
-            if (ca[i] != cb[i]) {
-                return false;
-            }
-        }
-
-        return true;
-    }
+        static boolean isAnagram(String s1, String s2) {
+		
+		int[] arr1 = new int[26];
+		int[] arr2 = new int[26];
+		for(char c: s1.toCharArray()) {
+			arr1[c-'a']++;
+		}
+		for(char c: s2.toCharArray()) {
+			arr2[c-'a']++;
+		}
+		for(int i =0; i<26; i++) {
+			if(arr1[i]!= arr2[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+  
 
 	
 	public static void main(String[] args) {
