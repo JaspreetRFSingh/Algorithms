e3c5b3f07dee173a82b010485718fad148cf6179
Jaspreet Singh <JaspreetRFSingh@users.noreply.github.com>
2018-12-21 00:20:38 +0530

    Merge Sort implementation using Integer arrays

---
+++ src/MergeSort.java
@@ -0,0 +1,65 @@
+
+public class MergeSort {
+
+	static void merge(int[] left, int[] right, int[] arr) {
+		int nl = left.length;
+		int nr = right.length;
+		int i =0, j=0, k=0;
+		while(i<nl && j<nr) {
+			if(left[i]<=right[j]) {
+				arr[k] = left[i];
+				i++;
+			}
+			else {
+				arr[k] = right[j];
+				j++;
+			}
+			k++;
+		}
+		while(i<nl) {
+			arr[k] = left[i];
+			i++;
+			k++;
+		}
+		while(j<nr) {
+			arr[k] = right[j];
+			j++;
+			k++;
+		}
+	}
+	
+	static void mergeSort(int[] arr) {
+		int n= arr.length;
+		if(n<2) {
+			return;
+		}
+		int mid = n/2;
+		int[] left = new int[mid];
+		int[] right = new int[n-mid];
+		
+		for(int i=0; i<mid; i++) {
+			left[i] = arr[i];
+		}
+		for(int i=mid; i<n; i++) {
+			right[i-mid]=arr[i];
+		}
+		mergeSort(left);
+		mergeSort(right);
+		merge(left, right, arr);
+	}
+	static void printArray(int[] arr) {
+		int n = arr.length;
+		for(int i=0 ;i <n; i++) {
+			System.out.print(arr[i]+" ");
+		}
+	}
+	
+	public static void main(String[] args) {
+		int [] arr = {23, 45, 67, 4, 12, 19, 89, 11, 9};
+		printArray(arr);
+		mergeSort(arr);
+		System.out.println();
+		printArray(arr);
+	}
+
+}
