69a670f73a8feee05e0e7fc07df17d2da1713909
Jaspreet Singh <JaspreetRFSingh@users.noreply.github.com>
2018-12-21 14:23:25 +0530

    Palindrome Checker

---
+++ src/Palindrome.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Palindrome {
+
+public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        String A=sc.next();
+
+        int n = A.length();
+        if(n==1) {
+        	System.out.println("Yes");
+        }
+        else {
+        	char[] data = A.toCharArray();
+            boolean isPali = false;
+            for(int i =0; i<n/2; i++){
+                isPali = false;
+                if(data[i]==data[n-1-i]){
+                    isPali = true;
+                }
+            }
+            if(isPali){
+                System.out.println("Yes");
+            }
+            else{
+                System.out.println("No");
+            }
+        }
+    }
+	
+}
